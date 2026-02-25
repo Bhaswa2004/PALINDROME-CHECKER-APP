6aa21755a2c274e16939e151a5903df777c8d8f1
Bhaswa2004 <debhaswardeep@gmail.com>
2026-02-25 15:44:40 +0530

    Base application setup

---
+++ src/PalindromeCheckerApp.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class PalindromeCheckerApp {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("================================");
+        System.out.println("      WELCOME TO PALINDROME     ");
+        System.out.println("================================");
+
+        System.out.println("Enter a word: ");
+        String word =scanner.nextLine();
+
+        String cleaned = word.replaceAll( "\\s+", "").toLowerCase();
+        String reversed = "";
+
+        for(int i = cleaned.length() - 1; i >= 0; i--) {
+            reversed = reversed + cleaned.charAt(i);
+        }
+        if(cleaned.equals(reversed)) {
+            System.out.println(word + "is a Palindrome");
+        } else {
+            System.out.println("is Not a Palindrome");
+        }
+        scanner.close();
+    }
+}
---
+++ src/uc1.java
@@ -0,0 +1,15 @@
+public class uc1 {
+    static final String APP_NAME = "Palindrome Checker App";
+    static final String APP_VERSION = "Version 1.0";
+
+    public static void main(String[] args) {
+
+        System.out.println("==================================");
+        System.out.println(" Welcome to " + APP_NAME);
+        System.out.println(" " + APP_VERSION);
+        System.out.println("==================================");
+
+        System.out.println("Application started successfully.");
+        System.out.println("Ready to check palindromes!");
+    }
+}
